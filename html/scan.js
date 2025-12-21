c9b6210f7d04c7f3ab8a4b8f8a5fa5442a7090ae
wvarty <wezley.varty@gmail.com>
2025-12-22 00:06:55 +1000

    Re-order tabs and fix spinner centering

--- html/scan.js
+++ html/scan.js
@@ -137,7 +137,6 @@ function updateConfig(data) {
         }
     }
     if((!data.stm32 || data.stm32==="no") && _('tx_tab')) {
-        mui.tabs.activate('pane-justified-2');
         _('tx_tab').style.display = 'none';
     }
     if(config['product_name'] && _('product_name')) _('product_name').textContent = config['product_name'];
